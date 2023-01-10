7c65f5eb8b469d1719b07ba08eab2b6c3cfa5082
Arthur Brière <arthur.briere@outlook.fr>
2023-01-10 09:19:38 +0100

    fix: handle bad piwa arguments with PiwaBadInputError

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,51 @@
-export type PiwaResponse<Data, Err> = Promise<
-  { data: Data; error: null } | { data: null; error: Err }
->;
-
-type PiwaArgs<Data> = Promise<Data> | (() => Promise<Data>) | (() => Data);
-
 /**
  * Promise wrapper that returns data or error in an object format
  * @param promiseOrFunction - Any promise, async function, or function
  */
-async function piwa<Data, Err = Error>(
+async function piwa<Data, Err extends Error>(
   promiseOrFunction: PiwaArgs<Data>
-): PiwaResponse<Data, Err> {
+): PiwaResponse<Data, PiwaError<Err>> {
+  const isValidInput =
+    typeof promiseOrFunction === 'function' ||
+    promiseOrFunction instanceof Promise;
+
+  if (!isValidInput) {
+    console.error(
+      '[Piwa] Invalid argument. Must be: a promise, an async function, or a function'
+    );
+    return { data: null, error: new PiwaBadInputError() };
+  }
+
   const _promise: Promise<Data> =
-    typeof promiseOrFunction === 'object'
+    promiseOrFunction instanceof Promise
       ? promiseOrFunction
-      : (async () => promiseOrFunction())();
+      : typeof promiseOrFunction === 'function'
+      ? (async () => promiseOrFunction())()
+      : promiseOrFunction;
 
   return _promise
     .then((data: Data) => ({ data, error: null }))
-    .catch(error => {
+    .catch((error: Err) => {
       return {
         error,
         data: null,
       };
     });
 }
+
+export class PiwaBadInputError extends Error {
+  constructor() {
+    super('You must provide a promise, async function, or function');
+    this.name = 'PiwaBadInputError';
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+  }
+}
+
+type PiwaError<Err = Error> = Err | PiwaBadInputError;
+type PiwaArgs<Data> = Promise<Data> | (() => Promise<Data>) | (() => Data);
+
+export type PiwaResponse<Data, Err extends Error> = Promise<
+  { data: Data; error: null } | { data: null; error: Err }
+>;
 
 export default piwa;
